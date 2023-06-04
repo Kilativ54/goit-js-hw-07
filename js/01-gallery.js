6e57dceeb33d08d2b26795ac0c06d4bdd4d58ac2
Kilativ54 <vitalikechkalov@gmail.com>
2023-06-04 18:00:07 +0200

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,10 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-const galleryContainer = document.querySelector('.gallery');
-const markup = galleryItems.map(({preview, original, description})=> `<li class="gallery__item">
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
 <a class="gallery__link" href='${original}'>
   <img
     class="gallery__image"
@@ -12,24 +13,25 @@ const markup = galleryItems.map(({preview, original, description})=> `<li class=
     alt='${description}'
   />
 </a>
-</li>`)
-galleryContainer.insertAdjacentHTML('beforeend', markup.join(''));
-galleryContainer.addEventListener('click', showPicture)
-function showPicture(event){
-    event.preventDefault();
-    if(event.target.nodeName !== 'IMG'){
-        return
-    };
-    const selectedImage = event.target.getAttribute('data-source')
+</li>`
+);
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+gallery.addEventListener("click", showPicture);
+function showPicture(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImage = event.target.getAttribute("data-source");
 
-    const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
     <img src="${selectedImage}" width="800" height="600">
-`)
+`);
 
-    instance.show()
-galleryContainer.addEventListener('keydown', event=>{
-    if(event.key === 'Escape'){
-        instance.close()
+  instance.show();
+  gallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
     }
-})
+  });
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+<a class="gallery__link" href='${original}'>
+  <img
+    class="gallery__image"
+    src='${preview}'
+    data-source='${original}'
+    alt='${description}'
+  />
+</a>
+</li>`
+);
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
