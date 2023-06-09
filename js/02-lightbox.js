6de6511ee249779174ae5f4e3fb037f6f3910ae2
Kilativ54 <vitalikechkalov@gmail.com>
2023-06-09 22:06:45 +0200

    cancel fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,7 +3,7 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
-const markup = gallery.map(
+const markup = galleryItems.map(
   ({ preview, original, description }) => `<li class="gallery__item">
 <a class="gallery__link" href='${original}'>
   <img
